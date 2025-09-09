9c075577fc273a260d592a7d69f159b8255e7a64
MatiXRay <matiasstrubbiamain@gmail.com>
2025-09-09 09:12:04 -0300

    se agregan imagen

---
+++ src/pages/Login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from "../assets/logo_mcraulo.svg"
+
+const Login = () => {
+    return (
+        <>
+            <h1>Login page</h1>
+            <img src={logo} alt="Logo McRaulo" className="w-100 h-100" />
+            <button>Ordenar sin asociar</button>
+
+        </>
+    )
+}
+
+export default Login
